bf2663d4488c4a212bb262e52bb487667abcb1a8
Talha-35 <tlh.34.bd@gmail.com>
2022-02-10 12:15:08 +0100

    updated toggle bar

--- JS/Main.js
+++ JS/Main.js
@@ -1,7 +1,18 @@
-const toggleButton = document.querySelector("#toggle-button");
+const toggleButtonOpen = document.querySelector("#toggle-button-open");
+const toggleButtonClose = document.querySelector("#toggle-button-close");
 const toggleNavbar = document.querySelector("#navbar-toggle");
-toggleButton.addEventListener("click", toggleFunction);
+const menuCizgi = document.querySelector("#menu-cizgi");
+const menuCarpi = document.querySelector("#menu-carpi");
+toggleButtonOpen.addEventListener("click", toggleOpen);
+toggleButtonClose.addEventListener("click", toggleClose);
 
-function toggleFunction() {
+function toggleOpen() {
     toggleNavbar.style.display = "block"
+    menuCizgi.style.display = "none"
+    menuCarpi.style.display = "block"
+}
+function toggleClose() {
+    toggleNavbar.style.display = "none"
+    menuCizgi.style.display = "block"
+    menuCarpi.style.display = "none"
 }
